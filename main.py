4cbe1381c197de7251f2531d732f8c189bcb83e3
Nate Brown <nate@nateb.dev>
2022-08-12 21:52:59 +0800

    fix client event

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
 
-@client.event()
+@client.event
 async def on_member_join(member):
     await member.create_dm()
     await member.dm_channel.send(
