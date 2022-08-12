5bab96de273b32ecbe9683d1c816f71b340a6049
Nate Brown <nate@nateb.dev>
2022-08-12 22:24:10 +0800

    fixed commands

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ async def hello(ctx):
 async def ping(ctx):
     await ctx.respond("Hey, are you there?")
 
-@bot.slash_command(guild_ids=testServer, name=['commands', 'cmds'])
+@bot.slash_command(guild_ids=testServer, name='commands')
 async def cmds(ctx):
     await ctx.respond("Here are the commands:")
     await ctx.respond("/hello - Says Hello")
