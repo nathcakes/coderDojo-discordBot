024383fdc5235def74205dea868a4edd1075ae08
Nate Brown <nate@nateb.dev>
2022-08-12 21:46:13 +0800

    updating for wokring commands

--- main.py
+++ main.py
@@ -22,7 +22,7 @@
 #This is a decorator implementation of the above
 import os
 import discord
-
+from discord.ext import commands
 
 client = discord.Client()
 
@@ -30,11 +30,23 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
 
-@client.event
+@client.event(guild_ids=[1004947709238718564])
 async def on_member_join(member):
     await member.create_dm()
     await member.dm_channel.send(
         f'Hi {member.name}, welcome to my Discord server!'
     )
+
+@client.slash_command(guild_ids=[1004947709238718564])
+async def hello(ctx):
+    await ctx.respond("Hello!")
+
+@client.slash_command(guild_ids=[1004947709238718564])
+async def ping(ctx):
+    await ctx.respond("Hey, are you there?")
+
+@client.command(guild_ids=[1004947709238718564], name='test')
+async def test(ctx,arg):
+    await ctx.send(arg)
 
 client.run(os.environ.get('DISCORD_TOKEN'))
