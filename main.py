6364b689f648ab37eb1c73d517cbaab2f49b77b9
Nate Brown <nate@nateb.dev>
2022-08-12 22:21:23 +0800

    fixed itents

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import os
 import discord
 from discord.ext import commands
 
-intents = discord.Intents.default()
-intents.members = True
-bot = discord.Bot()
-
+intents = discord.Intents.all()
+bot = discord.Bot(intents=intents)
+testServer = [1004947709238718564]
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
@@ -21,6 +20,13 @@ async def hello(ctx):
 @bot.slash_command(guild_ids=[1004947709238718564])
 async def ping(ctx):
     await ctx.respond("Hey, are you there?")
+
+@bot.slach_command(guild_ids=testServer, name=['commands', 'cmds'])
+async def cmds(ctx):
+    await ctx.respond("Here are the commands:")
+    await ctx.respond("/hello - Says Hello")
+    await ctx.respond("/ping - Says Hey, are you there?")
+    await ctx.respond("/test *args* - Repeats args back to you in the server")
 
 @bot.command(guild_ids=[1004947709238718564], name='test')
 async def test(ctx,arg):
